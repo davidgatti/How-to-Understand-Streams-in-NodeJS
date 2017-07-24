d8902c608fda95a6da5d7b1e7c8469fad8921746
Ahmed Shamim <shaon.cse81@gmail.com>
2017-07-24 16:25:15 +0600

    Code improvement in 'empty_custom_stream'

--- 4_empty_custom_stream/index.js
+++ 4_empty_custom_stream/index.js
@@ -1,20 +1,11 @@
-//
 // Load the modules
-//
-let fs = require('fs');
-let util = require("util");
-let stream = require('stream');
-let Transform = require("stream").Transform;
+const fs = require('fs');
+const util = require('util');
+const stream = require('stream');
+const Transform = stream.Transform;
 
-//
-//	Inherit Transform
-//
-util.inherits(OurDataManipulation, Transform);
-
-//
 //	Options for the file
-//
-let options_raw = {
+const options_raw = {
 	flags: 'r',
 	defaultEncoding: 'ascii',
 	fd: null,
@@ -22,50 +13,32 @@ let options_raw = {
 	autoClose: true
 }
 
-let options_compressed = {
-	flags: 'w',
-	defaultEncoding: 'ascii',
-	fd: null,
-	mode: 0o666,
-	autoClose: true
-}
+//	Open a file to be read
+const raw_file = fs.createReadStream('file.txt', options_raw);
 
-//
-//	Open a file to be red
-//
-let raw_file = fs.createReadStream('./file.txt', options_raw);
+//	Inherit Transform
+util.inherits(OurDataManipulation, Transform);
 
-//
+
 //	Create our custom stream processor
-//
 function OurDataManipulation () {
-
-	//
 	// invoke Transform constructor
-	//
     Transform.call(this);
 }
 
-//
-//	Create our custom stream that in this case doesn't do anything, it just
-//	shows what is the bare minimum needed to make a stream from scratch and
-//	pass the data to the next pipe.
-//
+/*
+	Create our custom stream that in this case doesn't do anything, it just
+	shows what is the bare minimum needed to make a stream from scratch and
+	pass the data to the next pipe.
+*/
 OurDataManipulation.prototype._transform = function (line, encoding, processed) {
-
-	//
 	//	Add the data that came in, to the output stream
-	//
 	this.push(line);
 
-	//
-	//	We let system know that we finished processing the data.
-	//
-    processed();
+	//	We let the system know that we finished processing the data.
+  processed();
 
 }
 
-//
 //	Pipe the data through our custom stream and then pass it to the console.
-//
 raw_file.pipe(new OurDataManipulation()).pipe(process.stdout);
