2e0a57a8e544d1707f18c3762af3fe07712f1780
Ahmed Shamim <shaon.cse81@gmail.com>
2017-07-24 13:16:29 +0600

    Some bug fixed and Code style improved

--- 1_read_from_stream/index.js
+++ 1_read_from_stream/index.js
@@ -1,50 +1,30 @@
-//
-// Load the modules
-//
-let fs = require('fs');
+/* Load the modules */
+const fs = require('fs');
 
-//
-//	Options for the file
-//
-let options = {
+/*	Options for the file */
+const options = {
 	flags: 'r',
 	defaultEncoding: 'ascii',
 	fd: null,
 	autoClose: true
 }
 
-//
-//	Open a file to be red
-//
-let file = fs.createReadStream('./file.txt', options);
+/*	Open a file to be read */
+const file = fs.createReadStream('file.txt', options);
 
-//
-//	Create a variable that will hold the amount of bytes red
-//
+/*	Create a variable that will hold the amount of bytes read */
 let size = 0;
 
-//
-//	Red the first set of the available buffer
-//
+/*	Read the first set of the available buffer */
 file.on('readable', function() {
-
-	console.log('-> Data available to be read')
-
-	//
-	//	Keep on reading until we get at the end of the file.
-	//
-	while(chunk = file.read(2000))
+	console.log('-> Data available to be read:')
+	/*	Keep on reading until we reach at the end of the buffer. */
+	while((chunk = file.read(2000)) != null)
 	{
-
-		//
-		//	Store the amount of bytes red
-		//
+		/*	Store the amount of bytes read */
 		size += chunk.length;
 
-		//
-		//	Display how many bytes did we get
-		//
-		console.log(`Received ${size} bytes of data.`);
+		/*	Display how many bytes we got*/
+		console.log(`Received ${size/1000} bytes of data.`);
 	}
-
 });
