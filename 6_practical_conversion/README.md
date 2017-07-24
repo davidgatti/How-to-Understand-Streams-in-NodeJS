09756bd2514a68df3d578c2917806d6e1006a73d
Ahmed Shamim <shaon.cse81@gmail.com>
2017-07-24 17:07:01 +0600

    Code improvement in '6_practical_conversion'

--- 6_practical_conversion/README.md
+++ 6_practical_conversion/README.md
@@ -2,8 +2,8 @@
 
 In this folder, you'll find the end result of what we learned in this article. Here you'll find two main files:
 
-- **make_comma_file.js**: This file uses the Chance module to create a txt file of a certain size (around 300KB). The file will be filled with the following data: `Name Last Name <email@exampl.ecom>, `. We're using our previous knowledge to write some content into a file using streams.
-- **converter.js**: This file is the interesting one. We're going to take the file created using `make_comma_file.js`, and create a new one with new content. We are going to change the `,` to `;`, so the end result will look like this: `Name Last Name <email@exampl.ecom>; `.
+- **make_comma_file.js**: This file uses the Chance module to create a txt file of a certain size (around 300KB). The file will be filled with the following data: `Name Last Name <email@example.com>, `. We're using our previous knowledge to write some content into a file using streams.
+- **converter.js**: This file is the interesting one. We're going to take the file created using `make_comma_file.js`, and create a new one with new content. We are going to change the `,` to `;`, so the end result will look like this: `Name Last Name <email@example.com>; `.
 
 
 # The better approach
@@ -11,10 +11,10 @@ In this folder, you'll find the end result of what we learned in this article. H
 I told you in the previous example that the approach used to manipulate the data is not the right one (a very inefficient approach). In the `converter.js` file, you'll see the following chunk of code, which manipulates the data in a very efficient way. We are going to work directly with the buffer (binary data).
 
 ```javascript
-//
-//	Loop over the buffer looking for ',' and replacing it with ';', which
-//	in ASCII ',' is 44 and ';' is 59
-//
+/*
+  Loop over the buffer looking for ',' and replacing it with ';', which
+  in ASCII ',' is 44 and ';' is 59
+*/
 for (let i = 0; i < buffer.length ; i++)
 {
 	if(buffer[i] == 44)
@@ -24,7 +24,7 @@ for (let i = 0; i < buffer.length ; i++)
 }
 ```
 
-The code is shorter, but this is not the point. The most important aspect is the fact that we are not converting the binary blob into ascii; we are editing the data right within the buffer.
+The most important aspect is the fact that we are not converting the binary blob into ascii; we are editing the data right within the buffer.
 
 # How to Run the Code
 
--- 6_practical_conversion/make_comma_file.js
+++ 6_practical_conversion/make_comma_file.js
@@ -1,18 +1,12 @@
-//
 // Load the modules
-//
-let fs = require('fs');
-let Chance = require('chance');
+const fs = require('fs');
+const Chance = require('chance');
 
-//
 // Instantiate Chance so it can be used
-//
-let chance = new Chance();
+const chance = new Chance();
 
-//
 //	Options for the file
-//
-let options = {
+const options = {
 	flags: 'w',
 	defaultEncoding: 'utf8',
 	fd: null,
@@ -20,73 +14,62 @@ let options = {
 	autoClose: true
 }
 
-//
 //	Execute the function that writes data in to the file.
-//
-let file = fs.createWriteStream('./comma_file.txt', options);
+const file = fs.createWriteStream('comma_file.txt', options);
 
-//
-//	Start he creation of the comma separated file
-//
-fill_the_file(file, true, 10000);
+//	Start the creation of the comma separated file
+fill_the_file(file, 10000);
 
-//
-//	Function that will write data in to the file.
-//
-//	file 	<- 	The file to write to
-//	ok		<- 	Decide whether you can write or not to the file
-//	size	<-	The loop size
-//
-function fill_the_file(file, ok, size)
+/*
+	Function that will write data in to the file.
+
+	file 	<- 	The file to write to
+	size	<-	The loop size
+*/
+function fill_the_file(file, size)
 {
+  const ok = true;
 
-	//
-	//	While the established size is bigger then zero and the buffer
-	//	didn't fill up.
-	//
+	/*
+		While the established size is bigger then zero
+    and the buffer didn't fill up.
+	*/
 	while(size-- && ok)
 	{
-		let first = chance.first();
-		let last = chance.last();
-		let email = chance.email({domain: 'example.com'});
+		const first = chance.first();
+		const last = chance.last();
+		const email = chance.email({domain: 'example.com'});
 
-		let string = `${first} ${last} <${email}>, `;
+		const string = `${first} ${last} <${email}>, `;
 
-		//
-		//	Write will set false once there is no more space to write,
-		//	meaning in the 'drain' event we need to set the size to + 1,
-		//	because write didn't write the data in the fail at the moment
-		//	of false.
-		//
-		ok = file.write(string, "ascii");
+		/*
+			Write will set false once there is no more space to write,
+			meaning in the 'drain' event we need to set the size to + 1,
+			because write didn't write the data in the fail at the moment
+			of false.
+		*/
+		ok = file.write(string, 'ascii');
 
-		//
 		//	Emmit when there is nothing more to write.
-		//
 		if(size === 0)
 		{
 			file.end();
 		}
 	}
 
-	//
-	//	If we didn't finish writing, we get hear because the buffer filled up,
-	//	and the .write() method notified us to resume writing after the
-	//	'drain' event is emitted.
-	//
+	/*
+		If we didn't finish writing, we get hear because the buffer filled up,
+		and the .write() method notified us to resume writing after the
+		'drain' event is emitted.
+	*/
 	if(size > 0)
 	{
-		//
-		//	Adds a one time listener function for the drain event so we
-		//	can be notified when the buffer became empty.
-		//
+		/*
+			Adds a one time listener function for the drain event so we
+			can be notified when the buffer became empty.
+		*/
 		file.once('drain', function() {
-
-			//
-			//	Resume writing
-			//
-			fill_the_file(file, true, size + 1)
-
+			fill_the_file(file, size + 1); //	Resume writing
 		});
 	}
 }
